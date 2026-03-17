cc014f823da0870a471fec6cdaa258e6b21ad9f0
Ku Lee Hann <leehannku@gmail.com>
2026-03-18 00:17:41 +0800

    tuto1q1

---
+++ tutorials/src/tuto1/q1/Main.java
@@ -0,0 +1,18 @@
+package tuto1.q1;
+
+//make sure the second arg has 8 numbers lol this thing aint advanced i lazy
+
+public class Main {
+    public static void main(String[] args) {
+        Telephone [] telephoneNum = new Telephone [5];
+        telephoneNum[0] = new Telephone(03,1234567); //why tf did it make it an octal number if i put 0 in front
+        telephoneNum[1] = new Telephone(03,1234569); //yea why
+        telephoneNum[2] = new Telephone(03,1234565); //yea whyy
+        telephoneNum[3] = new Telephone(03,91234567);
+        telephoneNum[4] = new Telephone(03,-71234567); //uh yea i love how crazy java numbers are, way too big -ve numbers somehow becomes +ve
+        for(int i = 0; i < 5; i++){
+            System.out.println(telephoneNum[i].makeFullNumber());
+        }
+    }
+
+}
---
+++ tutorials/src/tuto1/q1/Telephone.java
@@ -0,0 +1,45 @@
+package tuto1.q1;
+
+public class Telephone {
+    private int areaCode;
+    private int number;
+    private static int numberOfTelephoneObject = 0;
+
+    public Telephone(int areaCode, int number){
+        this.areaCode = areaCode;
+        this.number = number;
+        numberOfTelephoneObject++;
+    }
+
+    public int getAreaCode(){
+        return areaCode;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setAreaCode(int newAreaCode){
+        areaCode = newAreaCode;
+    }
+
+    public void setNumber(int newNumber){
+        number = newNumber;
+    }
+
+    public String makeFullNumber(){
+        StringBuilder sb = new StringBuilder();
+        if(areaCode < 10) sb.append(0);
+        sb.append(areaCode + "-");
+        if(number < 10000000){
+            int temp = number;
+            while(temp < 10000000){
+                sb.append(0);
+                temp*=10;
+            }
+        }
+        sb.append(number);
+        String fullNum = sb.toString();
+        return fullNum;
+    }
+}
